04bbd20184c12732cdcc3a97736cceec9993c9b3
Francis Vaz <vazfr@apac.linkgroup.corp>
2017-07-31 12:58:47 +1000

    completed the design of the card

--- src/app/admin/card/card.component.ts
+++ src/app/admin/card/card.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'card',
+  selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,33 @@
+import { CardComponent } from './admin/card/card.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import "@angular/material/prebuilt-themes/indigo-pink.css";
+import '@angular/material/prebuilt-themes/indigo-pink.css';
 import { ReactiveFormsModule } from '@angular/forms'
 
 import { MaterialModule } from '@angular/material';
 import { AdminModule } from './admin/admin.module';
 
 import 'hammerjs';
-import { TestService } from "app/test/test.service";
-import { HomeComponent } from "app/admin/home.component";
+import { TestService } from 'app/test/test.service';
+import { HomeComponent } from 'app/admin/home.component';
 import { AddComponent } from './admin/add/add.component';
-import { PersonService } from "app/admin/admin.service";
-import { HttpModule } from "@angular/http";
-import { ListComponent } from "app/admin/list/list.component";
+import { PersonService } from 'app/admin/admin.service';
+import { HttpModule } from '@angular/http';
+import { ListComponent } from 'app/admin/list/list.component';
 
 
 
-export const routes : Routes = [
-  { path:'', component: ListComponent},
-  { path:'home', component: TestComponent},
-  { path:'admin', component: HomeComponent},
-  { path:'admin/add', component: AddComponent},
-  { path:'admin/list', component: ListComponent}
+export const routes: Routes = [
+  { path: '', component: ListComponent},
+  { path: 'home', component: TestComponent},
+  { path: 'admin', component: HomeComponent},
+  { path: 'admin/add', component: AddComponent},
+  { path: 'admin/list', component: ListComponent},
+  { path: 'admin/card', component: CardComponent}
 ];
 
 @NgModule({
@@ -33,7 +35,8 @@ export const routes : Routes = [
     AppComponent,
     TestComponent,
     HomeComponent,
-    AddComponent
+    AddComponent,
+    CardComponent
   ],
   imports: [
     AdminModule,
