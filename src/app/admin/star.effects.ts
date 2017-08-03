eee1efef806e7a1b835d1442a4b00e1191539e9e
vazfr <vazfr@LG-AUD43303SK>
2017-08-03 16:38:48 +1000

    added effects but to do more reaserach as there are some function not currently working

--- src/app/admin/star.effects.ts
+++ src/app/admin/star.effects.ts
@@ -1,9 +1,26 @@
+import { StarService } from './star.service';
 import { Action } from 'rxjs/scheduler/Action';
+import { Observable } from 'rxjs/Rx';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, Effect } from '@ngrx/effects';
+import { ADD_STAR, EDIT_STAR, GET_STAR, EDIT_SUCCESS, EDIT_FAILED } from '../star.reducer';
 
 @Injectable()
 export class StarEffects {
 
-    constructor(private action$: Actions) { }
+    @Effect() edit$: Observable<any> = this.action$
+        .ofType(EDIT_STAR)
+        .map(a => a)
+        // .switchMap(payload => this._http.put('http://localhost:58215/api/stars/edit', payload)
+        .switchMap(payload => this._starService.edit(payload, 1))
+        .map(res => ({ type: EDIT_SUCCESS, payload: res }))
+        .catch(() => Observable.of({type: EDIT_FAILED}))
+        ;
+
+        constructor(
+        private action$: Actions,
+        private _http: HttpClient,
+        private _starService: StarService
+    ) { }
 }
--- src/app/star.reducer.ts
+++ src/app/star.reducer.ts
@@ -4,7 +4,8 @@ import { Action } from '@ngrx/store';
 export const ADD_STAR = 'ADD_STAR';
 export const EDIT_STAR = 'ADD_STAR';
 export const GET_STAR = 'GET_STAR';
-
+export const EDIT_SUCCESS = 'EDIT_SUCCESS';
+export const EDIT_FAILED = 'EDIT_FAILED';
 export function StarReducer(state: Star[] = [], action: Action) {
     console.log(state);
     switch (action.type) {
@@ -14,6 +15,10 @@ export function StarReducer(state: Star[] = [], action: Action) {
             return [...state, action]
         case GET_STAR:
             return [...state, action]
+        case EDIT_SUCCESS:
+            return state;
+        case EDIT_FAILED:
+            return state;
         default:
             return state;
     }
